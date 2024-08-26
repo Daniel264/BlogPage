7674190b0a025ffd13e7d3515643b3009ad94844
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-26 19:10:16 +0100

    Create page header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
-      <h1>test</h1>
+      <Header />
     </>
   );
 }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import  blogImage from '../images/blog-img.jpg'
+
+const header = () => {
+  return (
+    <>
+      <header className="flex justify-between">
+        <div>
+          <h1 className="font-bold text-4xl">MyBlog</h1>
+        </div>
+        <div className="flex justify-between w-40 font-medium text-lg">
+          <p>Login</p>
+          <p>Register</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default header;
