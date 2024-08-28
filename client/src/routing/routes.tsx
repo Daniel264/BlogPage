e631ca57a321fc333a83cb81470baabe914f1956
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-28 22:19:42 +0100

    Create CreatePost component and add it to routes

--- client/src/routing/routes.tsx
+++ client/src/routing/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import App from "../App";
+import CreatePost from "../components/CreatePost";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/Login", element: <Login /> },
   { path: "/Register", element: <Register /> },
+  { path: "/Create", element: <CreatePost /> },
 ]);
 
 export default router;
