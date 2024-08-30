228a91e5f4786a077547fc5e43eb2705bb81df23
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-30 22:40:21 +0100

    adding responsiveness to the website

--- client/src/components/BlogPage.tsx
+++ client/src/components/BlogPage.tsx
@@ -5,15 +5,17 @@ import { formatISO9075 } from "date-fns";
 const BlogPage = ({ title, summary, createdAt, author, cover }: Post) => {
   return (
     <div className="flex flex-col mt-10">
-      <div className="pl-5">
-        <h1 className="text-4xl font-semibold">{title}</h1>
-        <span>{author?.username}</span>
-        <time>{formatISO9075(new Date(createdAt))}</time>
+      <div className="pl-5 text-left">
+        <div className="flex flex-row w-full justify-between">
+          <span className="">{author?.username}</span>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </div>
+        <h1 className="text-2xl font-semibold">{title}</h1>
         <p>{summary}</p>
       </div>
-      <div className="w-full p-5">
+      <div className="w-full py-5 rounded-2xl">
         <img
-          className="w-full bg-cover h-[400px]"
+          className="w-full rounded-2xl bg-cover min-h-[192px] sm:h-[400px]"
           src={"http://localhost:3000/" + cover}
           alt=""
         />
