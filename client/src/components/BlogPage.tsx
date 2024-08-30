849c74fe4c9b444995dffe4080da27dd4fa79825
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-30 11:47:52 +0100

    use date-fns library to output file creation date

--- client/src/components/BlogPage.tsx
+++ client/src/components/BlogPage.tsx
@@ -1,17 +1,18 @@
 import blogImage from "../images/blog-img.jpg";
 import Post from "../assets/Interface/usePost";
+import {formatISO9075} from 'date-fns'
 
 const BlogPage = ({title, summary, content, cover, createdAt}: Post) => {
   return (
     <div className="flex mt-10">
       <div className="w-[70rem]">
-        <img className="w-max" src={content} alt="" />
+        <img className="w-max" src={ blogImage} alt="" />
       </div>
       <div className="pl-5">
         <h1 className="text-4xl font-semibold">
           {title}
         </h1>
-        <p>{createdAt}</p>
+        <time>{formatISO9075(new Date(createdAt))}</time>
         <p>
           {summary}
         </p>
