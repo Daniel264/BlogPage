5f5fed0c8719f704df216c0e6db105c392ee746c
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-30 22:28:42 +0100

    change the post container to be flex-col and image should fit

--- client/src/components/BlogPage.tsx
+++ client/src/components/BlogPage.tsx
@@ -4,19 +4,19 @@ import { formatISO9075 } from "date-fns";
 
 const BlogPage = ({ title, summary, createdAt, author, cover }: Post) => {
   return (
-    <div className="flex mt-10">
-      <div className="w-[70rem]">
-        <img
-          className="w-max"
-          src={"http://localhost:3000/" + cover}
-          alt=""
-        />
-      </div>
+    <div className="flex flex-col mt-10">
       <div className="pl-5">
         <h1 className="text-4xl font-semibold">{title}</h1>
         <span>{author?.username}</span>
         <time>{formatISO9075(new Date(createdAt))}</time>
         <p>{summary}</p>
+      </div>
+      <div className="w-full p-5">
+        <img
+          className="w-full bg-cover h-[400px]"
+          src={"http://localhost:3000/" + cover}
+          alt=""
+        />
       </div>
     </div>
   );
