88a8cafb763b7c80477d29cbd5dddab9e27a2a90
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-04 18:45:01 +0100

    Add type annotations to req,res

--- api/index.ts
+++ api/index.ts
@@ -1,32 +1,37 @@
-import express, { Request, Response } from "express";
-import multer, { Multer } from "multer";
-import fs from "fs";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import "dotenv/config";
-import "ts-node/register";
-import "tsconfig-paths/register";
 
-const app = express();
-const upload = multer({ dest: "uploads/" });
-
-const mongoURI = process.env.MONGODB_URI!;
-const secret = process.env.JWT_SECRET!;
-
-// Import your models
+import express from "express";
+import { Request, Response } from "express";
+import multer,{ Multer } from "multer";
 const Post = require("./models/Post");
 const Comment = require("./models/Comment");
 const Picture = require("./models/Picture");
+require("dotenv").config();
+import "ts-node/register";
+import "tsconfig-paths/register";
+
+
+const mongoURI = process.env.MONGODB_URI;
+const secret = process.env.JWT_SECRET;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
 const User = require("../api/models/User");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+// const multer = require("multer");
+const uploadMiddleware: Multer = multer({ dest: "uploads/" });
+const upload = multer();
+const fs = require("fs");
+
+// const secret = "hhfu8f7djfdlhijsfjuf78g7fvjfg";
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -84,37 +89,33 @@ app.post("/logout", (req: Request, res: Response) => {
   res.cookie("token", "").json("ok");
 });
 
-app.post(
-  "/post",
-  upload.single("file"),
-  async (req: Request, res: Response) => {
-    const file = req.file as Express.Multer.File | undefined;
-    if (!file) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-
-    const parts = file.originalname.split(".");
-    const extension = parts[parts.length - 1];
-    const newPath = `uploads/${file.filename}.${extension}`;
-    fs.renameSync(file.path, newPath);
-    const { token } = req.cookies;
-    if (!token) return res.status(401).json({ message: "Not authenticated" });
-    jwt.verify(token, secret, {}, async (err: Error, info: any) => {
-      if (err) throw err;
-
-      const { title, summary, content, comment } = req.body;
-      const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        comment,
-        cover: newPath,
-        author: info.id,
-      });
-      res.json(postDoc);
-    });
+app.post("/post", uploadMiddleware.single("file"), async (req: Request, res: Response) => {
+  const file = req.file as Express.Multer.File | undefined;
+  if (!file) {
+    return res.status(400).json({ message: "No file uploaded" });
   }
-);
+
+  const parts = file.originalname.split(".");
+  const extension = parts[parts.length - 1];
+  const newPath = `uploads/${file.filename}.${extension}`;
+  fs.renameSync(file.path, newPath);
+  const { token } = req.cookies;
+  if (!token) return res.status(401).json({ message: "Not authenticated" });
+  jwt.verify(token, secret, {}, async (err: Error, info: any) => {
+    if (err) throw err;
+
+    const { title, summary, content, comment } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      comment,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
 
 app.get("/post", async (req: Request, res: Response) => {
   const posts = await Post.find()
@@ -152,26 +153,22 @@ app.get("/comment", async (req: Request, res: Response) => {
   res.json(comments);
 });
 
-app.post(
-  "/picture",
-  upload.single("picture"),
-  async (req: Request, res: Response) => {
-    const file = req.file as Express.Multer.File | undefined;
-    if (!file) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-
-    const parts = file.originalname.split(".");
-    const extension = parts[parts.length - 1];
-    const newPath = `uploads/${file.filename}.${extension}`;
-    fs.renameSync(file.path, newPath);
-
-    const pictureDoc = await Picture.create({
-      picture: newPath,
-    });
-    res.json(pictureDoc);
+app.post("/picture", uploadMiddleware.single("picture"), async (req: Request, res: Response) => {
+  const file = req.file as Express.Multer.File | undefined;
+  if (!file) {
+    return res.status(400).json({ message: "No file uploaded" });
   }
-);
+
+  const parts = file.originalname.split(".");
+  const extension = parts[parts.length - 1];
+  const newPath = `uploads/${file.filename}.${extension}`;
+  fs.renameSync(file.path, newPath);
+
+  const pictureDoc = await Picture.create({
+    picture: newPath,
+  });
+  res.json(pictureDoc);
+});
 
 app.get("/picture", async (req: Request, res: Response) => {
   const picture = await Picture.find();
@@ -180,4 +177,12 @@ app.get("/picture", async (req: Request, res: Response) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port} `));
+
+// eL9wKRx5PyydyqLT
+//mongodb+srv://Daniel264:eL9wKRx5PyydyqLT@cluster0.q5k8t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// 2590682f-b507-440d-a2c5-4d61e41b040c
+
+// pass
+// SZJThMSYPE3FrF5Q
