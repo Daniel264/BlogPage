8b3872fe0c5ec5c60d46992cf5aff0b67452cac2
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-04 19:21:26 +0100

    delete all js files

--- dist/api/models/Picture.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const { Schema, model } = mongoose;
-const PictureSchema = new Schema({
-    picture: String,
-});
-const Picture = model("Picture", PictureSchema);
-module.exports = Picture;
