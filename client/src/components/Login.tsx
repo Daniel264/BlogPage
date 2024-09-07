0ea7eb13e798b80b4a23d03dbc55a1a012805332
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-07 02:59:52 +0100

    Remove Tailwind logo from Login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -50,11 +50,6 @@ const Login = () => {
       <Header />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 font-montserrat">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
             Welcome Back!
           </h2>
