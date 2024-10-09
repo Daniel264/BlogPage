e28edd1b36196560ceea5d317c42ac48b0e9d4ba
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-09 22:22:39 +0100

    Add react-toastify for notifications and update App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import BlogPage from "./components/BlogPage";
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import { useEffect, useState } from "react";
 import Post from "./assets/Interface/usePost";
@@ -8,30 +9,39 @@ import Post from "./assets/Interface/usePost";
 //https://blogpage-1-r5za.onrender.com
 
 function App() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/post").then((response) =>
-      response.json().then((posts) => setPosts(posts))
-    );
-  }, []);
+    const [posts, setPosts] = useState<Post[]>([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/post").then((response) =>
+            response.json().then((posts) => setPosts(posts)),
+        );
+    }, []);
 
-  return (
-    <Routes>
-      <Route
-        index
-        element={
-          <>
-            <Header />
-            {/* <h1 className="font-normal text-2xl md:text-5xl text-left mt-5">
+    return (
+        <>
+            <ToastContainer
+                newestOnTop={true}
+                pauseOnHover={false}
+                autoClose={3000}
+            />
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <>
+                            <Header />
+                            {/* <h1 className="font-normal text-2xl md:text-5xl text-left mt-5">
               Hi 👋
             </h1> */}
-            {posts.length > 0 &&
-              posts.map((post) => <BlogPage key={post.id} {...post} />)}
-          </>
-        }
-      />
-    </Routes>
-  );
+                            {posts.length > 0 &&
+                                posts.map((post) => (
+                                    <BlogPage key={post.id} {...post} />
+                                ))}
+                        </>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -75,6 +75,7 @@ const Header = () => {
                 >
                   {/* <svg 
                     xmlns="http://www.w3.org/2000/svg"
+                    
                     fill="none"
                     viewBox="0 0 24 24"
                     className="inline-block h-6 w-6 stroke-current"
