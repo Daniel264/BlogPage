837cf07408e0f10812bcc9c138e8dbdab831e472
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-12 22:02:36 +0100

    remove post and author parameters from comment.ts

--- api/models/Comment.ts
+++ api/models/Comment.ts
@@ -2,8 +2,8 @@ import mongoose, { Schema, model } from "mongoose";
 
 const CommentSchema = new Schema({
   content: { type: String, required: true },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  // post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+  // author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
