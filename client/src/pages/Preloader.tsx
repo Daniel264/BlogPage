d284a4302ba8d167cbe78896300b56ba1217d3ea
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-13 20:03:09 +0100

    wrap Preloader component in motion.div for animation support

--- client/src/pages/Preloader.tsx
+++ client/src/pages/Preloader.tsx
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion'
 
 const Preloader = () => {
     return (
-        <div className="w-full h-full flex items-center justify-center">
+        <motion.div className="w-full h-full flex items-center justify-center">
             <h1 className="font-semibold text-5xl">My Blog</h1>
-        </div>
+        </motion.div>
     );
 };
 
