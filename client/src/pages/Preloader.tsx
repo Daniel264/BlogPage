698100792f1ba1bfd236058f5e6ce8f5749bed00
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-13 18:39:57 +0100

    Create a preloader page and define its initial structure

---
+++ client/src/pages/Preloader.tsx
@@ -0,0 +1,12 @@
+
+import { motion } from 'framer-motion'
+
+const Preloader = () => {
+    return (
+        <div className="w-full h-full flex items-center justify-center">
+            <h1 className="font-semibold text-5xl">My Blog</h1>
+        </div>
+    );
+};
+
+export default Preloader;
