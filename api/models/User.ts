ddb919e90da7c16fef92ba26affb95eb847159da
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-13 18:58:10 +0100

    add a new schema in the user model called profile picture

--- api/models/User.ts
+++ api/models/User.ts
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String, required: false },
 });
 
 const UserModel = model("User", userSchema);
 
 module.exports = UserModel;
- 
