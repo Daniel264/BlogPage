9da45edcbcf4879eb0285eb21cd157d2433b5a36
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-13 18:46:02 +0100

    refactor registration page

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -41,13 +41,9 @@ const Register = () => {
 
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 font-montserrat">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
+   
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">
-                        Register for an account
+                        Register
                     </h2>
                 </div>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
