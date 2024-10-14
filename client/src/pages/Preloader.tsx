9a4c97d853dd21b9fbf94ede7daca36e6d68a017
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 09:54:58 +0100

    Redirect user to login page after preloader finishes

--- client/src/pages/Preloader.tsx
+++ client/src/pages/Preloader.tsx
@@ -1,7 +1,16 @@
-
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const Preloader = () => {
+    const [redirect, setRedirect] = useState(false);
+    setTimeout(() => {
+        setRedirect(true);
+    }, 8000);
+
+    if (redirect) {
+        return <Navigate to="/login" />;
+    }
     return (
         <motion.div className="w-full h-full flex items-center justify-center">
             <h1 className="font-semibold text-5xl">My Blog</h1>
