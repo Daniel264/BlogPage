91426f922c9d9f6249793acb38a0ab29e0ca5097
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 09:56:44 +0100

    Adjust routes to display preloader instead of animation component

--- client/src/routing/routes.tsx
+++ client/src/routing/routes.tsx
@@ -5,11 +5,12 @@ import App from "../App";
 import CreatePost from "../components/CreatePost";
 import PostPage from "../components/PostPage";
 import Settings from "../components/Settings";
-import AnimatePage from "../pages/AnimatePage";
+// import AnimatePage from "../pages/AnimatePage";
 import Profile from "../images/profile.png";
+import Preloader from "../pages/Preloader";
 
 const router = createBrowserRouter([
-  { path: "/", element: <AnimatePage /> },
+  { path: "/", element: <Preloader /> },
   { path: "/home", element: <App /> },
   { path: "/Login", element: <Login /> },
   { path: "/Register", element: <Register /> },
