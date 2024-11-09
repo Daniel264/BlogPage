abe2b2dde80b03c2a452c143069820ceb497295f
Daniel… <danieloluwafolajimi@gmail.com>
2024-11-09 23:43:22 +0100

    feat: enhance Tailwind CSS configuration with custom colors and synthwave theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,33 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        alexBrush: ['Alex Brush', 'cursive'],
-        roboto: ['Roboto', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif']
+      extend: {
+          colors: {
+              customBackground: "#272932", // Your custom color
+          },
+          fontFamily: {
+              alexBrush: ["Alex Brush", "cursive"],
+              roboto: ["Roboto", "sans-serif"],
+              montserrat: ["Montserrat", "sans-serif"],
+          },
+          fontWeight: {
+              light: 300,
+              regular: 400,
+              medium: 500,
+              bold: 700,
+              black: 900,
+          },
       },
-      fontWeight: {
-        light: 300,
-        regular: 400,
-        medium: 500,
-        bold: 700,
-        black: 900,
-
-      }
-    },
   },
   plugins: [
-    // eslint-disable-next-line no-undef
-    require('@tailwindcss/forms'),
-    // eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
-    require('daisyui'),
+      // eslint-disable-next-line no-undef
+      require("@tailwindcss/forms"),
+      // eslint-disable-next-line no-undef
+      require("daisyui"),
   ],
   daisyui: {
-    themes: ['light', 'synthwave'],
-  }
-}
+      themes: [
+          {
+              synthwave: {
+                  "primary": "#ff3eaa", // Customize colors directly here
+                  "secondary": "#c058f3",
+                  "accent": "#272932",
+                  "base-100": "#272932", // Your custom background color
+                  // You can add more color overrides here as needed
+              },
+          },
+          "light",
+      ],
+  },
+};
