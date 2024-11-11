53cfcf2359d4ea25e41928d0b506efa3b1749429
Daniel… <danieloluwafolajimi@gmail.com>
2024-11-11 20:44:17 +0100

    feat: add views tracking to posts and update Trending component to display cover images

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Trending() {
+    const [firstImage, setFirstImage] = useState<string[]>([]);
     async function Trending() {
         const response = await fetch("http://localhost:3000/post", {
             method: "GET",
@@ -8,7 +9,13 @@ export default function Trending() {
         });
         if (response.ok) {
             const result = await response.json();
-            console.log("result:", result);
+            console.log("result:", result[0]);
+            const coverImage = result[0];
+            // console.log("cover image", coverImage.cover);
+            setFirstImage(coverImage.cover);
+
+            const secondCoverImage = result[1];
+            setFirstImage(secondCoverImage)
         }
     }
     useEffect(() => {
@@ -20,8 +27,12 @@ export default function Trending() {
                 <p className="font-light text-4xl">Trending Posts.</p>
             </div>
             <div>
-                <div></div>
-                <div></div>
+                <div>
+                    <img src={`http://localhost:3000/${firstImage}`} alt="" />
+                </div>
+                <div>
+                <img src={`http://localhost:3000/${firstImage}`} alt="" />
+                </div>
                 <div></div>
             </div>
         </section>
