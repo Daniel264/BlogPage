826127a87cd1261291cad2221a062cc11173d2ec
Daniel… <danieloluwafolajimi@gmail.com>
2024-11-12 09:57:14 +0100

    refactor: stop the blog image from stretching to fill the div

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Trending() {
     const [firstImage, setFirstImage] = useState<string[]>([]);
+    const [titles, setTitles] = useState<string[]>([]);
     async function Trending() {
         const response = await fetch("http://localhost:3000/post", {
             method: "GET",
@@ -15,10 +16,13 @@ export default function Trending() {
                     b.views - a.views,
             );
             const topCovers = sortedResults.map(
-                (post: { cover: unknown }) => post.cover,
+                (post: { cover: unknown; title: string }) => post.cover,
+            );
+            const topTitles = sortedResults.map(
+                (post: { title: string }) => post.title,
             );
             setFirstImage(topCovers);
-            // console.log("Images", topCovers);
+            setTitles(topTitles);
         }
     }
     useEffect(() => {
@@ -26,30 +30,39 @@ export default function Trending() {
     }, []);
     return (
         <section className="w-full flex flex-col justify-center items-center">
-            <div>
+            {/* <div>
                 <h2 className="font-light text-5xl">Trending Posts.</h2>
-            </div>
+            </div> */}
             <div className="w-[50%] grid grid-cols-2 grid-rows-2 gap-5">
-                <div className="col-span-2 ">
+                <div className="col-span-2 relative h-[390px] w-[100%] flex justify-start items-end overflow-hidden">
                     <img
-                        className="h-[360px] w-[100%]"
+                        className="object-cover hover:scale-105  transition-all ease-in duration-300 overflow-hidden"
                         src={`http://localhost:3000/${firstImage[0]}`}
                         alt=""
                     />
+                    <p className="absolute text-white font-semibold text-2xl p-10">
+                        {titles[0]}
+                    </p>
                 </div>
-                <div className="col-span-1">
+                <div className="col-span-1 relative h-max w-auto flex justify-start items-end overflow-hidden">
                     <img
-                        className="h-[300px] w-[100%]"
+                        className="h-[220px] w-[100%] hover:scale-105  transition-all ease-in duration-300 overflow-hidden"
                         src={`http://localhost:3000/${firstImage[1]}`}
                         alt=""
                     />
+                    <p className="absolute text-white font-semibold text-xl p-6">
+                        {titles[1]}
+                    </p>
                 </div>
-                <div className="col-span-1">
+                <div className="col-span-1 relative h-max w-auto flex justify-start items-end overflow-hidden">
                     <img
-                        className="h-[300px] w-[100%]"
+                        className="h-[220px] w-[100%] hover:scale-105  transition-all ease-in duration-300 overflow-hidden"
                         src={`http://localhost:3000/${firstImage[2]}`}
                         alt=""
                     />
+                    <p className="absolute text-white font-semibold text-xl p-6">
+                        {titles[2]}
+                    </p>
                 </div>
             </div>
         </section>
